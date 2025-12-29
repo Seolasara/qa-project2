4a160427ea0dd684760bb54d35bba3163c62e712
Seolasara <seolakim195@gmail.com>
2025-12-29 12:02:07 +0900

    fix: 코드 수정

--- tests/api/test_object_storage.py
+++ tests/api/test_object_storage.py
@@ -248,7 +248,22 @@ class TestUserCRUD:
         assert response_json["id"] == user_id
 
 
-    def test_OS024_delete_user(self, api_headers, existing_user, base_url_object_storage):
+    @pytest.mark.xfail(reason="PATCH 요청에서 잘못된 필드 전달 시 200 OK 반환되는 문제")
+    def test_OS024_patch_invalid_field(self, api_headers, existing_user, base_url_object_storage):
+        user_id = existing_user["id"]
+        url = f"{base_url_object_storage}/user/{user_id}"
+        payload = {"id": "team2-01"}
+
+        response = requests.patch(url, headers=api_headers, json=payload)
+        # 상태 코드 검증
+        assert response.status_code == 422, f"⛔ [FAIL] 422와 다른 상태 코드: {response.status_code}"
+        response_json = response.json()
+        # 응답 바디 검증
+        assert isinstance(response_json, dict)
+        assert "not valid" in response_json["message"]
+
+
+    def test_OS025_delete_user(self, api_headers, existing_user, base_url_object_storage):
         user_id = existing_user["id"]
         url = f"{base_url_object_storage}/user/{user_id}"
 
@@ -260,3 +275,86 @@ class TestUserCRUD:
         assert isinstance(response_json, dict)
         assert response_json["id"] == user_id
         assert response_json["status"] == "deleting"
+
+
+class TestUserGrantCRUD:
+
+    @pytest.fixture
+    def existing_user_grant(self, api_headers, existing_bucket, existing_user, base_url_object_storage):
+        payload = {
+            "object_storage_id": existing_bucket["id"],
+            "object_storage_user_id": existing_user["id"],
+            "permission": "read_write",
+            "zone_id": "0a89d6fa-8588-4994-a6d6-a7c3dc5d5ad0"
+            }
+        
+        response = requests.post(f"{base_url_object_storage}/user_grant", headers=api_headers, json=payload)
+        assert response.status_code == 200, f"⛔ [FAIL] 사용자 권한 생성 실패 - {response.status_code}: {response.text}"
+        grant_id = response.json()["id"]
+        yield {"id": grant_id, "payload": payload}
+        requests.delete(f"{base_url_object_storage}/user_grant/{grant_id}", headers=api_headers)
+
+    # def test_OS011_post_user_grant(self, api_headers, existing_bucket, existing_user, base_url_object_storage):
+    #     bucket_id = existing_bucket["id"]
+    #     user_id = existing_user["id"]
+    #     payload = {
+    #         "object_storage_id": bucket_id,
+    #         "object_storage_user_id": user_id,
+    #         "permission": "read_write",
+    #         "zone_id": "0a89d6fa-8588-4994-a6d6-a7c3dc5d5ad0"
+    #         }
+
+    #     response = requests.post(f"{base_url_object_storage}/user_grant", headers=api_headers, json=payload)
+    #     # 상태 코드 검증
+    #     assert response.status_code == 200, f"⛔ [FAIL] 사용자 권한 생성 실패 - {response.status_code}: {response.text}"
+    #     # 응답 바디 검증
+    #     response_json = response.json()
+    #     assert isinstance(response_json, dict)
+    #     assert "id" in response_json
+    #     assert response_json["id"] is not None
+    #     requests.delete(f"{base_url_object_storage}/user_grant/{response_json['id']}", headers=api_headers)
+
+
+    def test_OS012_post_duplicate_user_grant(self, api_headers, existing_user_grant,base_url_object_storage):
+        payload = existing_user_grant["payload"]
+
+        response = requests.post(f"{base_url_object_storage}/user_grant", headers=api_headers, json=payload)
+        # 상태 코드 검증
+        assert response.status_code == 409, f"⛔ [FAIL] 409와 다른 상태 코드: {response.status_code}"
+        # 응답 바디 검증
+        response_json = response.json()
+        assert isinstance(response_json, dict)
+        assert "already exists" in response_json["message"]
+
+
+    def test_OS013_get_all_user(self, api_headers, existing_bucket, existing_user, base_url_object_storage):
+        url = f"{base_url_object_storage}/user?count=100"
+
+        response = requests.get(url, headers=api_headers)
+        # 상태 코드 검증
+        assert response.status_code == 200, f"⛔ [FAIL] 사용자 권한 목록 조회 실패 - 상태 코드: {response.status_code}"
+        # 응답 바디 검증
+        response_list = response.json()
+        assert isinstance(response_list, list)
+        assert len(response_list) > 0
+        assert response_list[0]["id"] is not None
+
+
+    def test_OS014_get_user_by_name(self, api_headers, existing_bucket, existing_user, base_url_object_storage):
+        user_id = existing_user["id"]
+        user_name = existing_user["name"]
+        url = f"{base_url_object_storage}/user?filter_name_like=%25{user_name}%25&count=100"
+
+        response = requests.get(url, headers=api_headers)
+        # 상태 코드 검증
+        assert response.status_code == 200, f"⛔ [FAIL] 사용자 권한 단건 조회 실패 - 상태 코드: {response.status_code}"
+        # 응답 바디 검증
+        response_json = response.json()
+        assert isinstance(response_json, list)
+        # 조회 사용자 검증
+        exact_user = [user for user in response_json if user["name"].stripi() == user_name]
+        assert len(exact_user) == 1
+        assert exact_user[0]["id"] == user_id
+        assert exact_user[0]["name"] == user_name
+        assert exact_user[0]["zone_id"] is not None
+        assert exact_user[0]["status"] == "activated"
